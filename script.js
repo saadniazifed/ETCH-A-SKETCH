a31bb284bc4b14765f3eb2cbe5023d4fbaaef865
Saad Niazi <saadniazifed@gmail.com>
2022-01-11 12:28:35 +0500

    Etch a Sketch project is now completed.

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 const container = document.querySelector("#container");
+const divCommonClass = document.querySelector(".divCommonClass");
 
-// Creating 256 Divs inside container using loop.
+// Creating 345 Divs inside container using loop.
 for (let i = 1; i <= 345; i++) {
   container.innerHTML += '<div class="divCommonClass"></div>';
 }
 
 // Adding Hover Effects to the Divs.
-// (But for this purpose I'm using a container. Because divs aren't working.)
 container.addEventListener("mouseover", (event) => {
   if (event.target.id == "container") {
     return;
   }
   event.target.style.backgroundColor = "royalblue";
 });
+
+// Creating a Reload button.
+const buttonReload = document.querySelector("#reloadButton");
+buttonReload.addEventListener("click", reload);
+
+function reload() {
+  location.reload();
+}
+
+// Generating a New Grid Layout.
+const newGrid = document.querySelector("#newGrid");
+newGrid.addEventListener("click", newGridLayout);
+
+function newGridLayout() {
+  let gridLayout = prompt("Enter the number of squares (Maximum 100): ");
+  if (gridLayout > 100) {
+    return;
+  }
+
+  const removeClass = document.querySelectorAll(".divCommonClass");
+  for (let i = 0; i < removeClass.length; i++) {
+    const element = removeClass[i];
+    element.parentNode.removeChild(element);
+  }
+  // Creating Custom Divs inside container using loop.
+  for (let i = 1; i <= gridLayout * gridLayout; i++) {
+    container.innerHTML += '<div class="divCommonClass"></div>';
+  }
+}
